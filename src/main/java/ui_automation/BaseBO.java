09517944fc649916f4456c9931355efc175b1bef
mykhailo.mikus <mykhailo.mikus@eleks.com>
2019-06-25 19:41:04 +0300

    add sell bot close selenide

--- src/main/java/ui_automation/BaseBO.java
+++ src/main/java/ui_automation/BaseBO.java
@@ -1,11 +1,15 @@
 package ui_automation;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.google.common.collect.ImmutableMap;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import static com.codeborne.selenide.Configuration.*;
@@ -27,19 +31,21 @@ public class BaseBO {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
         Capabilities capabilities = new Capabilities() {
             @Override
             public Map<String, ?> asMap() {
+                System.out.println("asMap "+options.asMap());
                 return options.asMap();
             }
 
             @Override
             public Object getCapability(String s) {
+                System.out.println("getCapability "+s);
                 return options.asMap().get(s);
             }
         };
-        browserCapabilities.merge(capabilities);
-
+        Configuration.browserCapabilities.merge(capabilities);
     }
 
     void goToPath(String path) {
