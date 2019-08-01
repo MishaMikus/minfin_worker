827f68739984c988557ea0c7607639d19c7f8098
mykhailo.mikus <mykhailo.mikus@eleks.com>
2019-08-01 15:02:16 +0300

    add sell buy price form

--- src/main/java/ui_automation/minfin/bo/LoginBO.java
+++ src/main/java/ui_automation/minfin/bo/LoginBO.java
@@ -9,24 +9,14 @@ public class LoginBO extends BaseMinfinBO {
         goToPath("/signout");
         goToPath("/login");
         try {
-            //<input class="mfm-auth--input" type="text" placeholder="Логин" name="Login" autofocus="" required="">
+            System.out.println("LOGIN : " + login + " " + pass);
             $(By.xpath("/html/body/main/div/div/div[1]/div/div/div/div/form/div[1]/input")).setValue(login);
             $(By.xpath("/html/body/main/div/div/div[1]/div/div/div/div/form/div[2]/input")).setValue(pass);
             $(By.xpath("/html/body/main/div/div/div[1]/div/div/div/div/form/div[4]/button")).click();
-
-//            executeJavaScriptAction("SET LOGIN", "document.getElementsByName(\"Login\")[1].value='" + login + "';");
-//            executeJavaScriptAction("SET LOGIN", "document.getElementsByName(\"Login\")[2].value='" + login + "';");
-//            executeJavaScriptAction("SET LOGIN", "document.getElementsByName(\"Login\")[3].value='" + login + "';");
-//
-//            executeJavaScriptAction("SET PASS", "document.getElementsByName(\"Password\")[1].value='" + pass + "';");
-//            executeJavaScriptAction("SET PASS", "document.getElementsByName(\"Password\")[2].value='" + pass + "';");
-//            executeJavaScriptAction("SET PASS", "document.getElementsByName(\"Password\")[3].value='" + pass + "';");
-//            executeJavaScriptAction("SUBMIT", "document.querySelectorAll(\".mfm-auth--submit-btn[type=submit]\")[1].click();");
-//            executeJavaScriptAction("SUBMIT", "document.querySelectorAll(\".mfm-auth--submit-btn[type=submit]\")[2].click();");
-//            executeJavaScriptAction("SUBMIT", "document.querySelectorAll(\".mfm-auth--submit-btn[type=submit]\")[3].click();");
+            System.out.println("LOGIN DONE");
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.print("could'nt login");
+            System.out.print("CAN'T LOGIN");
             return false;
         }
         return true;
