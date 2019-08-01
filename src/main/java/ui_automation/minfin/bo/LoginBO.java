277860b64e3abf248392be1b471860d1c2d97e29
mykhailo.mikus <mykhailo.mikus@eleks.com>
2019-08-01 14:28:36 +0300

    add sell buy price form

--- src/main/java/ui_automation/minfin/bo/LoginBO.java
+++ src/main/java/ui_automation/minfin/bo/LoginBO.java
@@ -2,12 +2,14 @@ package ui_automation.minfin.bo;
 
 public class LoginBO extends BaseMinfinBO {
     public boolean login(String login, String pass) {
+        goToPath("/signout");
         goToPath("/login");
         try {
             executeJavaScriptAction("SET LOGIN", "document.getElementsByName(\"Login\")[3].value='" + login + "';");
             executeJavaScriptAction("SET PASS", "document.getElementsByName(\"Password\")[3].value='" + pass + "';");
             executeJavaScriptAction("SUBMIT", "document.querySelectorAll(\".mfm-auth--submit-btn[type=submit]\")[3].click();");
         } catch (Exception e) {
+            e.printStackTrace();
             System.out.print("could'nt login");
             return false;
         }
