8664eb15ce99e333b67e22bb6a95f1fcb42bfba3
mykhailo.mikus <mykhailo.mikus@eleks.com>
2019-08-08 13:46:52 +0300

    refreshPrice

--- src/main/java/ui_automation/minfin/Trader.java
+++ src/main/java/ui_automation/minfin/Trader.java
@@ -46,10 +46,12 @@ public class Trader {
         System.out.println("START refreshPrice");
         try {
             String sellContent = HTMLUtil.getContentByURL("https://minfin.com.ua/currency/auction/usd/sell/lvov");
+            System.out.println("sellContent : "+sellContent);
             Double price_sell = getAverageFirst10Prices(RegExpUtil.findsByRegex(sellContent, PRICE_REGEX, 1));
             new SellPriceDAO().save(new SellPrice(price_sell));
 
             String buyContent = HTMLUtil.getContentByURL("https://minfin.com.ua/currency/auction/usd/buy/lvov");
+            System.out.println("buyContent : "+buyContent);
             Double price_buy = getAverageFirst10Prices(RegExpUtil.findsByRegex(buyContent, PRICE_REGEX, 1));
             new BuyPriceDAO().save(new BuyPrice(price_buy));
 
@@ -64,6 +66,7 @@ public class Trader {
         priceList = priceList.subList(0, 10);
         Integer count = 0;
         for (String price : priceList) {
+            System.out.println("getAverageFirst10Prices : "+price);
             average += Double.parseDouble(price.replaceAll(",", "."));
             count++;
         }
