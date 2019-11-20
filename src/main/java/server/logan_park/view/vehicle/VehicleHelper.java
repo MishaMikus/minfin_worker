ba01fd77530d031bba5b4564205bb2bdbcf5e260
mykhailo.mikus <mykhailo.mikus@eleks.com>
2019-11-20 17:55:15 +0200

    fix filling vehicle helper

--- src/main/java/server/logan_park/view/vehicle/VehicleHelper.java
+++ src/main/java/server/logan_park/view/vehicle/VehicleHelper.java
@@ -38,21 +38,23 @@ public class VehicleHelper {
     public void addVehicle(VehicleView vehicleView) {
         Vehicle vehicle = makeVehicle(vehicleView);
         int id = (int) VehicleDAO.getInstance().save(vehicle);
-        FillingCard fillingCard = new FillingCard();
+        FillingCard fillingCard;
         if (vehicleView.getUpgCard() != null) {
             fillingCard = FillingCardDAO.getInstance().findById(vehicleView.getUpgCard());
             if (fillingCard != null) {
                 fillingCard.setId(vehicleView.getUpgCard());
+                fillingCard.setVehicle_id(id);
+                FillingCardDAO.getInstance().saveOrUpdate(fillingCard);
             }
         }
         if (vehicleView.getOkkoCard() != null) {
             fillingCard = FillingCardDAO.getInstance().findById(vehicleView.getOkkoCard());
             if (fillingCard != null) {
                 fillingCard.setId(vehicleView.getOkkoCard());
+                fillingCard.setVehicle_id(id);
+                FillingCardDAO.getInstance().saveOrUpdate(fillingCard);
             }
         }
-        fillingCard.setVehicle_id(id);
-        FillingCardDAO.getInstance().saveOrUpdate(fillingCard);
     }
 
     private Vehicle makeVehicle(VehicleView vehicleView) {
